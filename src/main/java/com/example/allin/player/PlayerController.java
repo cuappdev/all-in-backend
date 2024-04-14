ea134fff1bf837a66dd31d1cdb4fce360d0fddd2
akm99 <akm99@cornell.edu>
2024-04-14 15:34:27 -0400

    Implement player model

--- src/main/java/com/example/allin/player/PlayerController.java
+++ src/main/java/com/example/allin/player/PlayerController.java
@@ -1,1 +1,65 @@
 package com.example.allin.player;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.allin.exceptions.NotFoundException;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class PlayerController {
+  public final PlayerService playerService;
+
+  public PlayerController(PlayerService playerService) {
+    this.playerService = playerService;
+  }
+
+  @GetMapping("/players/")
+  public ResponseEntity<List<Player>> getAllPlayers() {
+    List<Player> players = playerService.getAllPlayers();
+    return ResponseEntity.ok(players);
+  }
+
+  @GetMapping("/players/{playerId}/")
+  public ResponseEntity<Player> getPlayerById(@PathVariable final Integer playerId) {
+    try {
+      Player player = playerService.getPlayerById(playerId);
+      return ResponseEntity.ok(player);
+    } catch (NotFoundException e) {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PostMapping("/players/")
+  public ResponseEntity<Player> createPlayer(@RequestBody final Player player) {
+    return ResponseEntity.status(201).body(playerService.createPlayer(player));
+  }
+
+  @PatchMapping("/players/{playerId}/")
+  public ResponseEntity<Player> updatePlayer(@PathVariable final Integer playerId,
+      @RequestBody final Player player) {
+    try {
+      Player updatedPlayer = playerService.updatePlayer(playerId, player);
+      return ResponseEntity.ok(updatedPlayer);
+    } catch (NotFoundException e) {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @DeleteMapping("/players/{playerId}/")
+  public ResponseEntity<Player> deletePlayer(@PathVariable final Integer playerId) {
+    try {
+      return ResponseEntity.ok(playerService.deletePlayer(playerId));
+    } catch (NotFoundException e) {
+      return ResponseEntity.notFound().build();
+    }
+  }
+}
--- src/main/java/com/example/allin/player/PlayerService.java
+++ src/main/java/com/example/allin/player/PlayerService.java
@@ -1,1 +1,63 @@
 package com.example.allin.player;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.allin.exceptions.NotFoundException;
+
+@Service
+public class PlayerService {
+
+  private final PlayerRepo playerRepo;
+
+  public PlayerService(PlayerRepo playerRepo) {
+    this.playerRepo = playerRepo;
+  }
+
+  public List<Player> getAllPlayers() {
+    return playerRepo.findAll();
+  }
+
+  public Player getPlayerById(final Integer player_id) throws NotFoundException {
+    Optional<Player> playerOptional = playerRepo.findById(player_id);
+    if (playerOptional.isEmpty()) {
+      throw new NotFoundException();
+    }
+    return playerOptional.get();
+  }
+
+  public Player createPlayer(final Player player) {
+    return playerRepo.save(player);
+  }
+
+  public Player updatePlayer(final Integer player_id, final Player player) throws NotFoundException {
+    Optional<Player> playerOptional = playerRepo.findById(player_id);
+    if (playerOptional.isEmpty()) {
+      throw new NotFoundException();
+    }
+    Player playerToUpdate = playerOptional.get();
+    playerToUpdate.setFirstName(player.getFirstName());
+    playerToUpdate.setLastName(player.getLastName());
+    playerToUpdate.setPosition(player.getPosition());
+    playerToUpdate.setNumber(player.getNumber());
+    playerToUpdate.setHeight(player.getHeight());
+    playerToUpdate.setWeight(player.getWeight());
+    playerToUpdate.setYear(player.getYear());
+    playerToUpdate.setHometown(player.getHometown());
+    playerToUpdate.setHighSchool(player.getHighSchool());
+    playerToUpdate.setImage(player.getImage());
+    playerToUpdate.setBio(player.getBio());
+    return playerRepo.save(playerToUpdate);
+  }
+
+  public Player deletePlayer(final Integer player_id) throws NotFoundException {
+    Optional<Player> playerOptional = playerRepo.findById(player_id);
+    if (playerOptional.isEmpty()) {
+      throw new NotFoundException();
+    }
+    playerRepo.deleteById(player_id);
+    return playerOptional.get();
+  }
+}
