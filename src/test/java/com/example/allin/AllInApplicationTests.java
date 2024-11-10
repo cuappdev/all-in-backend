6fb832f973e6abf0580cc8739636152f835caedf
cindy-x-liang <67083541+cindy-x-liang@users.noreply.github.com>
2024-11-10 15:55:42 -0500

    added factories

--- src/test/java/com/example/allin/AllInApplicationTests.java
+++ src/test/java/com/example/allin/AllInApplicationTests.java
@@ -6,13 +6,15 @@ import com.example.allin.contract.Contract;
 import com.example.allin.contract.util.ContractGenerator;
 import com.example.allin.contract.Rarity;
 import com.example.allin.player.Player;
-import com.example.allin.player.Position;
 import com.example.allin.user.User;
 import com.example.allin.playerData.PlayerDataRepo;
+import com.example.allin.data.PlayerFactory;
+import com.example.allin.data.UserFactory;
+
+
 
 class AllInApplicationTests {
-
-	@Test
+    @Test
   public void testGenerateContract() {
     
     PlayerDataRepo playerDataRepo = mock(PlayerDataRepo.class);
@@ -36,21 +38,21 @@ class AllInApplicationTests {
     System.out.println("Legendary Probability: [" + ContractGenerator.LEGENDARY_PROB_LOWER_BOUND + ", " + ContractGenerator.LEGENDARY_PROB_UPPER_BOUND + "]");
     System.out.println("Legendary Payout: [" + ContractGenerator.LEGENDARY_PAYOUT_LOWER_BOUND + ", " + ContractGenerator.LEGENDARY_PAYOUT_UPPER_BOUND + "]\n");
 
-		double common_alpha = (ContractGenerator.COMMON_PROB_UPPER_BOUND + ContractGenerator.COMMON_PROB_LOWER_BOUND) * (ContractGenerator.COMMON_PAYOUT_UPPER_BOUND + ContractGenerator.COMMON_PAYOUT_LOWER_BOUND) / 4.0;
-		double rare_alpha = (ContractGenerator.RARE_PROB_UPPER_BOUND + ContractGenerator.RARE_PROB_LOWER_BOUND) * (ContractGenerator.RARE_PAYOUT_UPPER_BOUND + ContractGenerator.RARE_PAYOUT_LOWER_BOUND) / 4.0;
-		double epic_alpha = (ContractGenerator.EPIC_PROB_UPPER_BOUND + ContractGenerator.EPIC_PROB_LOWER_BOUND) * (ContractGenerator.EPIC_PAYOUT_UPPER_BOUND + ContractGenerator.EPIC_PAYOUT_LOWER_BOUND) / 4.0;
-		double legendary_alpha = (ContractGenerator.LEGENDARY_PROB_UPPER_BOUND + ContractGenerator.LEGENDARY_PROB_LOWER_BOUND) * (ContractGenerator.LEGENDARY_PAYOUT_UPPER_BOUND + ContractGenerator.LEGENDARY_PAYOUT_LOWER_BOUND) / 4.0;
+    double common_alpha = (ContractGenerator.COMMON_PROB_UPPER_BOUND + ContractGenerator.COMMON_PROB_LOWER_BOUND) * (ContractGenerator.COMMON_PAYOUT_UPPER_BOUND + ContractGenerator.COMMON_PAYOUT_LOWER_BOUND) / 4.0;
+    double rare_alpha = (ContractGenerator.RARE_PROB_UPPER_BOUND + ContractGenerator.RARE_PROB_LOWER_BOUND) * (ContractGenerator.RARE_PAYOUT_UPPER_BOUND + ContractGenerator.RARE_PAYOUT_LOWER_BOUND) / 4.0;
+    double epic_alpha = (ContractGenerator.EPIC_PROB_UPPER_BOUND + ContractGenerator.EPIC_PROB_LOWER_BOUND) * (ContractGenerator.EPIC_PAYOUT_UPPER_BOUND + ContractGenerator.EPIC_PAYOUT_LOWER_BOUND) / 4.0;
+    double legendary_alpha = (ContractGenerator.LEGENDARY_PROB_UPPER_BOUND + ContractGenerator.LEGENDARY_PROB_LOWER_BOUND) * (ContractGenerator.LEGENDARY_PAYOUT_UPPER_BOUND + ContractGenerator.LEGENDARY_PAYOUT_LOWER_BOUND) / 4.0;
 
-		System.out.println("Alphas:");
-		System.out.println("Common: " + common_alpha);
-		System.out.println("Rare: " + rare_alpha);
-		System.out.println("Epic: " + epic_alpha);
-		System.out.println("Legendary: " + legendary_alpha + "\n");
+    System.out.println("Alphas:");
+    System.out.println("Common: " + common_alpha);
+    System.out.println("Rare: " + rare_alpha);
+    System.out.println("Epic: " + epic_alpha);
+    System.out.println("Legendary: " + legendary_alpha + "\n");
 
     System.out.println("Generating a Contract\n");
-    
-    User user = new User("Tony", "akm99", "src/main/resources/static/images/users/default.jpg", 1000.0);
-    Player player = new Player("LeBron", "James", new Position[]{Position.Center}, 23, "6'9", 250, "Akron, OH", "St. Vincent-St. Mary", "src/main/resources/static/images/players/default.jpg");
+
+    User user = UserFactory.createRandomUser();
+    Player player = PlayerFactory.createFakePlayer();
     Contract contract = contractGenerator.generateContract(user, player, 100.0, Rarity.Common);
     System.out.println("Contract: " + contract.toString());
   }
--- src/test/java/com/example/allin/data/PlayerFactory.java
+++ src/test/java/com/example/allin/data/PlayerFactory.java
@@ -1,16 +1,41 @@
 package com.example.allin.data;
 import com.example.allin.player.Player;
 import com.example.allin.player.Position;
+import com.github.javafaker.Faker;
+import java.util.Random;
+
 
 public class PlayerFactory {
-  
-  public static Player fakeTemplate() {
-    return new Player("LeBron", "James", new Position[]{Position.Center}, 23, "6'9", 250, "Akron, OH", "St. Vincent-St. Mary", "src/main/resources/static/images/players/default.jpg");
+  private static final Faker faker = new Faker();
+  private static final Random random = new Random();
+
+
+  public static Player createFakePlayer() {
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    Position[] positions = generateRandomPositions();
+    Integer number = faker.number().numberBetween(1, 99);  // Jersey numbers typically range from 1-99
+    String height = generateRandomHeight();
+    Integer weight = faker.number().numberBetween(150, 250);  // Weight in pounds, adjustable as needed
+    String hometown = faker.address().city() + ", " + faker.address().state();
+    String highSchool = faker.educator().secondarySchool();
+     return new Player(firstName, lastName, positions, number, height, weight, hometown, highSchool);
+    }
+
+  private static Position[] generateRandomPositions() {
+      Position[] allPositions = Position.values();
+      int count = random.nextInt(2) + 1; // Players can have 1 or 2 positions
+      Position[] positions = new Position[count];
+      for (int i = 0; i < count; i++) {
+          positions[i] = allPositions[random.nextInt(allPositions.length)];
+      }
+      return positions;
   }
 
-  public static Player fake() {
-    // return a player with random values
-    return new Player("LeBron", "James", new Position[]{Position.Center}, 23, "6'9", 250, "Akron, OH", "St. Vincent-St. Mary", "src/main/resources/static/images/players/default.jpg");
-  }
+  private static String generateRandomHeight() {
+    int feet = faker.number().numberBetween(5, 7); // Heights between 5 and 7 feet
+    int inches = faker.number().numberBetween(0, 11);
+    return feet + "'" + inches + "\"";
+}
 
 }
