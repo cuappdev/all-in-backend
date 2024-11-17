e10bbd8e15fa2fa3a00d47a1f77b702d9465ec48
skyeslattery <dss354@cornell.edu>
2024-11-17 16:05:22 -0500

    add player scrapers

--- src/main/java/com/appdev/allin/Application.java
+++ src/main/java/com/appdev/allin/Application.java
@@ -1,11 +1,16 @@
 package com.appdev.allin;
 
-import com.appdev.allin.player.PlayerRepo;
-import com.appdev.allin.playerData.PlayerDataRepo;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.appdev.allin.player.PlayerRepo;
+import com.appdev.allin.playerData.PlayerDataRepo;
+import com.appdev.allin.playerData.util.PopulatePlayerData;
+
+import scrapers.PlayerDataScraper;
 
 @SpringBootApplication
 public class Application {
@@ -17,4 +22,17 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Bean
+    public CommandLineRunner populatePlayers(PlayerRepo playerRepo, PlayerDataRepo playerDataRepo) {
+            return (args) -> {
+                    PlayerDataScraper playerDataScraper = new PlayerDataScraper(playerRepo);
+                    playerDataScraper.populate();
+
+
+                    PopulatePlayerData populatePlayerData = new PopulatePlayerData(playerRepo, playerDataRepo);
+                    populatePlayerData.run();
+            };
+    }
 }
+
--- src/main/java/com/appdev/allin/playerData/PlayerDataRepo.java
+++ src/main/java/com/appdev/allin/playerData/PlayerDataRepo.java
@@ -1,19 +1,16 @@
 package com.appdev.allin.playerData;
 
-import com.appdev.allin.player.Player;
+import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
+import com.appdev.allin.player.Player;
 
 @Repository
 public interface PlayerDataRepo extends JpaRepository<PlayerData, Integer> {
-
     List<PlayerData> findByPlayer(Player player);
-
     List<PlayerData> findByGameDate(LocalDate gameDate);
-
-    PlayerData findByPlayerAndGameDate(Player player, LocalDate gameDate);
+    List<PlayerData> findByPlayerAndGameDate(Player player, LocalDate gameDate);
 }
