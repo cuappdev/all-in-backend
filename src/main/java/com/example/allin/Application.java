78d0392766f6e3c9232d7b1c3848b34e92943c03
Ashley Herrera <ashley@dhcp-vl2041-19243.eduroam.cornell.edu>
2024-11-18 21:43:32 -0500

    feat: game data scraper

--- src/main/java/com/example/allin/Application.java
+++ src/main/java/com/example/allin/Application.java
@@ -10,7 +10,11 @@ import com.example.allin.player.PlayerRepo;
 import com.example.allin.playerData.PlayerDataRepo;
 import com.example.allin.playerData.util.PopulatePlayerData;
 
+import com.example.allin.gameData.GameDataRepo;
+
 import scrapers.PlayerDataScraper;
+import scrapers.GameDataScraper;
+
 
 @SpringBootApplication
 public class Application {
@@ -21,10 +25,23 @@ public class Application {
         @Autowired
         PlayerDataRepo playerDataRepo;
 
+        @Autowired
+        GameDataRepo gameDataRepo;
+
+
         public static void main(String[] args) {
                 SpringApplication.run(Application.class, args);
         }
 
+        @Bean
+        public CommandLineRunner populateGameData(GameDataRepo gameDataRepo) {
+                return args -> {
+                        System.out.println("Starting GameDataScraper!");
+                        GameDataScraper gameDataScraper = new GameDataScraper(gameDataRepo);
+                        gameDataScraper.run();
+                };
+        }
+        
         @Bean
         public CommandLineRunner populatePlayers(PlayerRepo playerRepo, PlayerDataRepo playerDataRepo) {
                 return (args) -> {
@@ -35,5 +52,5 @@ public class Application {
                         populatePlayerData.run();
                 };
         }
-
+        
 }
--- src/main/java/com/example/allin/gameData/GameData.java
+++ src/main/java/com/example/allin/gameData/GameData.java
@@ -1,202 +1,89 @@
 package com.example.allin.gameData;
 
 
-import com.example.allin.contract.Event;
-import com.example.allin.contract.OpposingTeam;
-import com.example.allin.player.Player;
 import com.example.allin.gameData.GameData;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
-
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
-
-import java.time.LocalDate;
-
 
 @Entity
 @Table(name = "game_data")
-// game id, gameDate, game time, opposing team, location (which school), location (actual building)
 public class GameData {
-   @Id
-   @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
-   private Integer id;
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "opposingTeam", nullable = false)
+    private String opposingTeam;
+ 
+    @Column(name = "gameDateTime", nullable = false)
+    private String gameDateTime;
+
+    @Column(name = "fullLocation", nullable = false)
+    private String fullLocation;
+
+    @Column(name= "logoUrl", nullable = false)
+    private String logoUrl;
 
 
-   @ManyToOne(fetch = FetchType.LAZY)
-   @JoinColumn(name = "game_id")
-   @OnDelete(action = OnDeleteAction.CASCADE)
-   private Game game;
-
-
-   @Column(name = "opposingTeam", nullable = false)
-   private OpposingTeam opposingTeam;
-
-
-   @Column(name = "opposingTeamLogo", nullable = false)
-   private OpposingTeam opposingTeamLogo;
-
-
-   @Column(name = "gameDate", nullable = false)
-   private LocalDate gameDate;
-
-
-   @Column(name = "gameTime", nullable = false)
-   private DateTime gameTime;
-
-
-   @Column(name = "collegeCampus", nullable = false)
-   private Location collegeCampus;
-
-
-   @Column(name = "actualLocation", nullable = false)
-   private Location actualLocation;
-
-
-   public GameData() {}
-
-
-   public GameData(
-           Game game,
-           OpposingTeam opposingTeam,
-           OpposingTeam opposingTeamLogo,
-           LocalDate gameDate,
-           DateTime gameTime,
-           Location collegeCampus,
-           Location actualLocation
-           ) {
-       this.game = game;
-       this.opposingTeam = opposingTeam;
-       this.opposingTeamLogo = opposingTeamLogo;
-       this.gameDate = gameDate;
-       this.gameTime = gameTime;
-       this.collegeCampus = collegeCampus;
-       this.actualLocation = actualLocation;
-   }
-
-
-   // game id
-   public Integer getId() {
-       return id;
-   }
-
-
-   public void setId(Integer id) {
-       this.id = id;
-   }
-
-
-   // opposing team
-   public OpposingTeam getOpposingTeam() {
-       return opposingTeam;
-   }
-
-
-   public void setOpposingTeam(OpposingTeam opposingTeam) {
-       this.opposingTeam = opposingTeam;
-   }
-
-    // opposing team logo
-   public OpposingTeam getOpposingTeamLogo() {
-       return opposingTeamLogo;
-   }
-
-
-   public void setOpposingTeamLogo(OpposingTeam opposingTeamLogo) {
-       this.opposingTeamLogo = opposingTeamLogo;
-   }
+    public GameData() {}
   
-   // game date
-   public LocalDate getGameDate() {
-       return gameDate;
-   }
+    public GameData(
+        String opposingTeam,
+        String gameDateTime,
+        String fullLocation,
+        String logoUrl
 
+        ) {
+        this.opposingTeam = opposingTeam;
+        this.gameDateTime = gameDateTime;
+        this.fullLocation = fullLocation;
+        this.logoUrl = logoUrl;
+    }
 
-   public void setGameDate(LocalDate gameDate) {
-       this.gameDate = gameDate;
-   }
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    // game time
-   public DateTime getGameTime() {
-       return gameTime;
-   }
+    public String getOpposingTeam() {
+        return opposingTeam;
+    }
 
+    public void setOpposingTeam(String opposingTeam) {
+        this.opposingTeam = opposingTeam;
+    }
 
-   public void setGameTime(DateTime gameTime) {
-       this.gameTime = gameTime;
-   }
-  
-    // collegeCampus
-    public Location getCollegeCampus() {
-       return collegeCampus;
-   }
+    public String getGameDateTime() {
+        return gameDateTime;
+    }
 
+    public void setGameDateTime(String gameDateTime) {
+        this.gameDateTime = gameDateTime;
+    }
 
-   public void setCollegeCampus(Location collegeCampus) {
-       this.collegeCampus = collegeCampus;
-   }
+    public String getFullLocation() {
+        return fullLocation;
+    }
 
-    // actualLocation
-    public Location getActualLocation() {
-       return actualLocation;
-   }
+    public void setFullLocation(String fullLocation) {
+        this.fullLocation = fullLocation;
+    }
+    
+    public String getLogoUrl() {
+        return logoUrl;
+    }
 
-   public void setActualLocation(Location actualLocation) {
-       this.actualLocation = actualLocation;
-   }
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
 
-
-   @Override
-   public String toString() {
-       return "GameData [id="
-               + id
-               + ", opposingTeam="
-               + opposingTeam
-               + ", opposingTeamLogo="
-               + opposingTeamLogo
-               + ", gameDate="
-               + gameDate
-               + ", gameTime="
-               + gameTime
-               + ", collegeCampus="
-               + collegeCampus
-               + ", actualLocation="
-               + actualLocation
-               + "]";
-   }
-
-
-   public Integer getEvent(Event e) {
-       switch (e) {
-           case Game:
-               return getId();
-           case OpposingTeam:
-               return getOpposingTeam();
-           case OpposingTeamLogo:
-               return getOpposingTeamLogo();
-           case GameDate:
-               return getGameDate();
-           case GameTime:
-               return getGameTime();
-           case CollegeCampus:
-               return getCollegeCampus();
-           case ActualLocation:
-               return getActualLocation();
-           default:
-               return 0;
-       }
-   }
 }
-
-
